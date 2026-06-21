4703294287c789d26856a97afea63e0c4b722ee0
Peyrass <alejandro.pires@live.u-tad.com>
2026-06-21 23:03:38 +0200

    Última Build

--- Assets/Scripts/UI/Pausa/SC_ButtonSound.cs
+++ Assets/Scripts/UI/Pausa/SC_ButtonSound.cs
@@ -5,20 +5,21 @@ namespace UI.Pausa
 {
     public class SC_ButtonSound : MonoBehaviour, IPointerEnterHandler, ISelectHandler
     {
-        [Header("Sonidos UI")]
-        [SerializeField] private AudioSource uiAudioSource; 
+        [Header("Sonidos UI")] [SerializeField]
+        private AudioSource uiAudioSource;
+
         [SerializeField] private AudioClip hoverSound;
-    
+
         public void OnPointerEnter(PointerEventData eventData)
         {
             PlaySound();
         }
-    
+
         public void OnSelect(BaseEventData eventData)
         {
             PlaySound();
         }
-    
+
         private void PlaySound()
         {
             if (uiAudioSource != null && hoverSound != null)
